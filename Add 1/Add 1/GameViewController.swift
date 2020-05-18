9b7f066a6f454306107cc9f8fcb28b7ccb537c8c
Abins Musthafa <abins.musthafa@innovaturelabs.com>
2020-05-18 18:23:33 +0530

    created the main story bord

---
+++ Add 1/Add 1/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Add 1	
+//
+//  Created by Jerald Jacob on 18/05/20.
+//  Copyright © 2020 Innovature Labs. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
